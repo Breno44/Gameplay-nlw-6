b359f228905ca7d8cf830b662338d78d5dfce19f
Breno <bhfs.2908@gmail.com>
2021-11-03 19:12:18 -0300

    feat:class 3

---
+++ src/screens/Guilds/index.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import { View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { Guild, GuildProps } from "../../components/Guild";
+import { ListDivider } from "../../components/ListDivider";
+
+import { styles } from "./styles";
+
+type Props = {
+  handleGuildsSelect: (guild: GuildProps) => void;
+};
+
+export function Guilds({ handleGuildsSelect }: Props) {
+  const guilds = [
+    {
+      id: "1",
+      name: "Lendários",
+      icon: "icon.png",
+      owner: true,
+    },
+    {
+      id: "2",
+      name: "Amigos Do Neymar",
+      icon: "icon.png",
+      owner: true,
+    },
+    {
+      id: "3",
+      name: "Indianos",
+      icon: "icon.png",
+      owner: true,
+    },
+    {
+      id: "4",
+      name: "Burgueses",
+      icon: "icon.png",
+      owner: true,
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={guilds}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Guild data={item} onPress={() => handleGuildsSelect(item)} />}
+        ItemSeparatorComponent={() => <ListDivider />}
+        style={styles.guilds}
+      />
+    </View>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -10,9 +10,13 @@ import { CategorySelect } from "../../components/CategorySelect";
 import { ListHeader } from "../../components/ListHeader";
 import { Appointment } from "../../components/Appointment";
 import { ListDivider } from "../../components/ListDivider";
+import { Background } from "../../components/Background";
+import { useNavigation } from "@react-navigation/native";
 
 export function Home() {
   const [category, setCategory] = useState("");
+
+  const navigation = useNavigation();
 
   const appointments = [
     {
@@ -45,11 +49,19 @@ export function Home() {
     categoryId === category ? setCategory("") : setCategory(categoryId);
   }
 
+  function handleAppointmentDetails() {
+    navigation.navigate("AppointmentDetails");
+  }
+
+  function handleAppointmentCreate() {
+    navigation.navigate("AppointmentCreate");
+  }
+
   return (
-    <View>
+    <Background>
       <View style={styles.header}>
         <Profile />
-        <ButtonAdd />
+        <ButtonAdd onPress={handleAppointmentCreate} />
       </View>
 
       <CategorySelect categorySelected={category} setCategory={handleCategorySelect} />
@@ -60,12 +72,12 @@ export function Home() {
         <FlatList
           data={appointments}
           keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <Appointment data={item} />}
+          renderItem={({ item }) => <Appointment data={item} onPress={handleAppointmentDetails} />}
           style={styles.matches}
           ItemSeparatorComponent={() => <ListDivider />}
           showsVerticalScrollIndicator={false}
         />
       </View>
-    </View>
+    </Background>
   );
 }
